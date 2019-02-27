09ce1602be5daa423a6fb53c3719d85f2a35cbdc
ferrymen <ferrymen33@gmail.com>
2019-02-27 21:46:30 +0800

    https->http

--- Core/src/main/java/com/ferrymen/core/common/BaseConstant.kt
+++ Core/src/main/java/com/ferrymen/core/common/BaseConstant.kt
@@ -2,8 +2,9 @@ package com.ferrymen.core.common
 
 class BaseConstant {
     companion object {
-//        var SERVER_ADDRESS = "http://127.0.0.1:8080"
-        var SERVER_ADDRESS = "https://a9178972.ngrok.io"
+        // https://blog.csdn.net/jolin678/article/details/50664281/
+        var SERVER_ADDRESS = "http://10.0.2.2:2002" // http://127.0.0.1:2002
+//        var SERVER_ADDRESS = "https://a9178972.ngrok.io"
 //        var SERVER_ADDRESS = "https://fm-android-kotlin-api.localmachine"
 //        var SERVER_ADDRESS = "https://localhost:3002"
         const val TABLE_PREFS = "shop"
