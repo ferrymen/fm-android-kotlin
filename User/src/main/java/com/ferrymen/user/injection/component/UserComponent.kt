cd1c8602b8d3f4825fa1dca32f1eefcf88a6d3b1
ferrymen <ferrymen33@gmail.com>
2019-03-02 09:53:01 +0800

    add User::ForgetPwd injection layer

--- User/src/main/java/com/ferrymen/user/injection/component/UserComponent.kt
+++ User/src/main/java/com/ferrymen/user/injection/component/UserComponent.kt
@@ -3,6 +3,7 @@ package com.ferrymen.user.injection.component
 import com.ferrymen.core.injection.PerComponentScope
 import com.ferrymen.core.injection.component.ActivityComponent
 import com.ferrymen.user.injection.module.UserModule
+import com.ferrymen.user.ui.activity.ForgetPwdActivity
 import com.ferrymen.user.ui.activity.LoginActivity
 import com.ferrymen.user.ui.activity.RegisterActivity
 import dagger.Component
@@ -12,4 +13,5 @@ import dagger.Component
 interface UserComponent {
     fun inject(activity: RegisterActivity)
     fun inject(activity: LoginActivity)
+    fun inject(activity: ForgetPwdActivity)
 }
