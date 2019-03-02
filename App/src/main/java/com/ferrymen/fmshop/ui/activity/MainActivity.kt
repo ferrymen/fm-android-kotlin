22023a724f0d98f4717a91796c31b8021c2aa21d
ferrymen <ferrymen33@gmail.com>
2019-03-02 18:46:02 +0800

    show HomeFragment

--- App/src/main/java/com/ferrymen/fmshop/ui/activity/MainActivity.kt
+++ App/src/main/java/com/ferrymen/fmshop/ui/activity/MainActivity.kt
@@ -3,6 +3,7 @@ package com.ferrymen.fmshop.ui.activity
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import com.ferrymen.fmshop.R
+import com.ferrymen.fmshop.ui.fragment.HomeFragment
 import kotlinx.android.synthetic.main.activity_main.*
 import rx.Observable
 import rx.android.schedulers.AndroidSchedulers
@@ -25,5 +26,13 @@ class MainActivity : AppCompatActivity() {
 //                .observeOn(AndroidSchedulers.mainThread())
 //                .subscribe({mBottomNavBar.checkCartBadge(0)})
 
+        initView()
+
+    }
+
+    private fun initView() {
+        var manager = supportFragmentManager.beginTransaction()
+        manager.replace(R.id.mContainer, HomeFragment())
+        manager.commit()
     }
 }
--- App/src/main/java/com/ferrymen/fmshop/ui/fragment/HomeFragment.kt
+++ App/src/main/java/com/ferrymen/fmshop/ui/fragment/HomeFragment.kt
@@ -5,9 +5,17 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.ferrymen.core.ui.fragment.BaseFragment
+import com.ferrymen.fmshop.R
 
 class HomeFragment: BaseFragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return super.onCreateView(inflater, container, savedInstanceState)
+        super.onCreateView(inflater, container, savedInstanceState)
+        val rootView = inflater.inflate(R.layout.fragment_home, null)
+
+        initView()
+        return rootView
+    }
+
+    private fun initView() {
     }
 }
