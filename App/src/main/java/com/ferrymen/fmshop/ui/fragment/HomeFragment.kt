176af55b0753b46f05fa02aef67e67772cf51c81
ferrymen <ferrymen33@gmail.com>
2019-03-02 19:36:23 +0800

    fixed mHomeBanner is null

--- App/src/main/java/com/ferrymen/fmshop/ui/fragment/HomeFragment.kt
+++ App/src/main/java/com/ferrymen/fmshop/ui/fragment/HomeFragment.kt
@@ -15,17 +15,22 @@ import kotlinx.android.synthetic.main.fragment_home.*
 import org.jetbrains.anko.find
 
 class HomeFragment: BaseFragment() {
-    private lateinit var mHomeBanner: Banner
+//    private lateinit var mHomeBanner: Banner
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         super.onCreateView(inflater, container, savedInstanceState)
-        val rootView = inflater.inflate(R.layout.fragment_home, null)
-
-        initBanner(rootView)
-        return rootView
+//        val rootView = inflater.inflate(R.layout.fragment_home, null)
+//
+//        initBanner(rootView)
+//        return rootView
+        return inflater.inflate(R.layout.fragment_home, null)
     }
 
-    private fun initBanner(view: View) {
-        mHomeBanner = view.find(R.id.mHomeBanner)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initBanner()
+    }
+
+    private fun initBanner() {
         mHomeBanner.setImageLoader(BannerImageLoader())
         mHomeBanner.setImages(listOf(
                 HOME_BANNER_ONE,
@@ -37,4 +42,18 @@ class HomeFragment: BaseFragment() {
         mHomeBanner.setIndicatorGravity(BannerConfig.RIGHT)
         mHomeBanner.start()
     }
+
+//    private fun initBanner(view: View) {
+//        mHomeBanner = view.find(R.id.mHomeBanner)
+//        mHomeBanner.setImageLoader(BannerImageLoader())
+//        mHomeBanner.setImages(listOf(
+//                HOME_BANNER_ONE,
+//                HOME_BANNER_TWO,
+////                HOME_BANNER_THREE,
+//                HOME_BANNER_FOUR))
+//        mHomeBanner.setBannerAnimation(Transformer.Accordion)
+//        mHomeBanner.setDelayTime(2000)
+//        mHomeBanner.setIndicatorGravity(BannerConfig.RIGHT)
+//        mHomeBanner.start()
+//    }
 }
