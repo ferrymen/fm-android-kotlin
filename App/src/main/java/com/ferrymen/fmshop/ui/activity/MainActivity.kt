840f2093547ed2faa230ccfeeed4964633a47f94
ferrymen <ferrymen33@gmail.com>
2019-03-02 17:42:45 +0800

    BottomNav Badge

---
+++ App/src/main/java/com/ferrymen/fmshop/ui/activity/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.ferrymen.fmshop.ui.activity
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.ferrymen.fmshop.R
+import kotlinx.android.synthetic.main.activity_main.*
+import rx.Observable
+import rx.android.schedulers.AndroidSchedulers
+import java.util.*
+import java.util.concurrent.TimeUnit
+
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        mBottomNavBar.checkMsgBadge(false)
+        mBottomNavBar.checkCartBadge(20)
+        Observable.timer(2, TimeUnit.SECONDS)
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe({mBottomNavBar.checkMsgBadge(true)})
+        Observable.timer(5, TimeUnit.SECONDS)
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe({mBottomNavBar.checkCartBadge(0)})
+
+    }
+}
