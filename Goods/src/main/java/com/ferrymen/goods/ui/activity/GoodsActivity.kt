ac14e3d21f76ff7f0cc3d294fdda5ffe662187f3
ferrymen <ferrymen33@gmail.com>
2019-03-03 16:44:02 +0800

    Module::Goods Dagger inject

--- Goods/src/main/java/com/ferrymen/goods/ui/activity/GoodsActivity.kt
+++ Goods/src/main/java/com/ferrymen/goods/ui/activity/GoodsActivity.kt
@@ -1,14 +1,27 @@
 package com.ferrymen.goods.ui.activity
 
+import android.os.Bundle
 import com.ferrymen.core.ui.activity.BaseMVPActivity
 import com.ferrymen.goods.data.protocol.Goods
+import com.ferrymen.goods.injection.component.DaggerGoodsComponent
+import com.ferrymen.goods.injection.module.GoodsModule
 import com.ferrymen.goods.presenter.GoodsListPresenter
 import com.ferrymen.goods.presenter.view.GoodsListView
 
 class GoodsActivity: BaseMVPActivity<GoodsListPresenter>(), GoodsListView {
     override fun injectComponent() {
+        DaggerGoodsComponent.builder().activityComponent(activityComponent).goodsModule(GoodsModule()).build().inject(this)
+        mPresenter.mView = this
     }
 
     override fun onGetGoodsListResult(result: MutableList<Goods>?) {
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        initView()
+    }
+
+    private fun initView() {
     }
 }
