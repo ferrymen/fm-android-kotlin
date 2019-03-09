9e49942ba8668babd08f2ef8b4756669ba83d0ac
ferrymen <ferrymen33@gmail.com>
2019-03-09 09:11:15 +0800

    Module::Goods Cart GoodsComponent dependencies on CartModule

--- Goods/src/main/java/com/ferrymen/goods/injection/component/GoodsComponent.kt
+++ Goods/src/main/java/com/ferrymen/goods/injection/component/GoodsComponent.kt
@@ -2,6 +2,7 @@ package com.ferrymen.goods.injection.component
 
 import com.ferrymen.core.injection.PerComponentScope
 import com.ferrymen.core.injection.component.ActivityComponent
+import com.ferrymen.goods.injection.module.CartModule
 import com.ferrymen.goods.injection.module.CategoryModule
 import com.ferrymen.goods.injection.module.GoodsModule
 import com.ferrymen.goods.ui.activity.GoodsActivity
@@ -10,7 +11,7 @@ import com.ferrymen.goods.ui.fragment.GoodsDetailTabOneFragment
 import dagger.Component
 
 @PerComponentScope
-@Component(dependencies = arrayOf(ActivityComponent::class),modules = arrayOf(GoodsModule::class))
+@Component(dependencies = arrayOf(ActivityComponent::class),modules = arrayOf(GoodsModule::class, CartModule::class))
 interface GoodsComponent {
     fun inject(activity: GoodsActivity)
     fun inject(fragment: GoodsDetailTabOneFragment)
