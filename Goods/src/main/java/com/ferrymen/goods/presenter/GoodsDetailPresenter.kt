042e7d4bf8e7d33989c7cd114eafc70259a73ded
ferrymen <ferrymen33@gmail.com>
2019-03-09 09:17:38 +0800

    Module::Goods Cart presenter layer

--- Goods/src/main/java/com/ferrymen/goods/presenter/GoodsDetailPresenter.kt
+++ Goods/src/main/java/com/ferrymen/goods/presenter/GoodsDetailPresenter.kt
@@ -3,8 +3,11 @@ package com.ferrymen.goods.presenter
 import com.ferrymen.core.ext.execute
 import com.ferrymen.core.presenter.BasePresenter
 import com.ferrymen.core.rx.BaseSubscriber
+import com.ferrymen.core.widgets.AppPrefsUtils
+import com.ferrymen.goods.common.GoodsConstant
 import com.ferrymen.goods.data.protocol.Goods
 import com.ferrymen.goods.presenter.view.GoodsDetailView
+import com.ferrymen.goods.service.CartService
 import com.ferrymen.goods.service.GoodsService
 import javax.inject.Inject
 
@@ -15,6 +18,9 @@ class GoodsDetailPresenter @Inject constructor() : BasePresenter<GoodsDetailView
 
     @Inject
     lateinit var goodsService: GoodsService
+
+    @Inject
+    lateinit var cartService: CartService
 
 //    @Inject
 //    lateinit var cartService: CartService
@@ -38,20 +44,20 @@ class GoodsDetailPresenter @Inject constructor() : BasePresenter<GoodsDetailView
     /*
         加入购物车
      */
-//    fun addCart(goodsId: Int, goodsDesc: String, goodsIcon: String, goodsPrice: Long,
-//                           goodsCount: Int, goodsSku: String) {
-//        if (!checkNetWork()) {
-//            return
-//        }
-//        mView.showLoading()
-//        cartService.addCart(goodsId,goodsDesc,goodsIcon,goodsPrice,
-//                goodsCount,goodsSku).excute(object : BaseSubscriber<Int>(mView) {
-//            override fun onNext(t: Int) {
-//                AppPrefsUtils.putInt(GoodsConstant.SP_CART_SIZE,t)
-//                mView.onAddCartResult(t)
-//            }
-//        }, lifecycleProvider)
-//
-//    }
+    fun addCart(goodsId: Int, goodsDesc: String, goodsIcon: String, goodsPrice: Long,
+                           goodsCount: Int, goodsSku: String) {
+        if (!checkNetWork()) {
+            return
+        }
+        mView.showLoading()
+        cartService.addCart(goodsId,goodsDesc,goodsIcon,goodsPrice,
+                goodsCount,goodsSku).execute(object : BaseSubscriber<Int>(mView) {
+            override fun onNext(t: Int) {
+                AppPrefsUtils.putInt(GoodsConstant.SP_CART_SIZE,t)
+                mView.onAddCartResult(t)
+            }
+        }, lifecycleProvider)
+
+    }
 
 }
